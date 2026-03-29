085bf48edc8875da8003b5f7354762491aee6668
Daniel Medrano Huerta <daniel@quicktortoise.com>
2026-03-28 20:54:23 -0600

    Sort imports

--- src/TurnSystem.Frontend/src/App.tsx
+++ src/TurnSystem.Frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import TurnMachine from './pages/TurnMachine';
 import RoleRunner from './pages/RoleRunner';
+import TurnMachine from './pages/TurnMachine';
 
 function App() {
 
