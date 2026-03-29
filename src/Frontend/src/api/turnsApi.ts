ae2e4e9e7e595d94f52048bec51b23f655f8cee1
Daniel Medrano Huerta <daniel@quicktortoise.com>
2026-03-29 12:56:35 -0600

    Refactor and expand API functionality
    - Split `api.ts` into `turnsApi.ts` and `runnersApi.ts` for better organization.
    - Added new API methods: `getAssignations`, `assignTurn`, `getRunner`, `enroll`, `unenroll`, and `toggleRunner`.
    - Improved error handling for API requests.

---
+++ src/Frontend/src/api/turnsApi.ts
@@ -0,0 +1,47 @@
+import { axiosClient } from "./axiosClient.ts";
+import type { Turn } from "../models/Turn.ts";
+import type { TurnsSummary } from "../models/TurnsSummary.ts";
+import axios from "axios";
+import type { TurnAssignment } from "../models/TurnAssignment.ts";
+import type { AssignmentSummary } from "../models/AssignmentSummary.ts";
+
+export async function newTurn(type: string) {
+  try {
+    const response = await axiosClient.post<Turn>(`/turns?role=${type}`);
+    return response.data;
+  } catch (error) {
+    throw new Error("Unknown error when requesting a turn", { cause: error });
+  }
+}
+
+export async function getTurns() {
+  try {
+    const response = await axiosClient.get<TurnsSummary>("/turns");
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 400) throw new Error("Invalid status");
+    throw new Error("Unknown error when getting turns", { cause: error });
+  }
+}
+
+export async function getAssignations() {
+  try {
+    const response = await axiosClient.get<AssignmentSummary[]>("/turns/assign");
+    return response.data;
+  } catch (error) {
+    throw new Error("Unknown error when getting assignations", { cause: error });
+  }
+}
+
+export async function assignTurn(runnerId: string) {
+  try {
+    const response = await axiosClient.post<TurnAssignment>(`/turns/assign/${runnerId}`);
+    if (response.status === 200) return response.data;
+    return null;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 400) throw new Error("Runner is inactive");
+    if (axios.isAxiosError(error) && error.response?.status === 404) throw new Error("Runner not found");
+    throw new Error("Unknown error when requesting a turn", { cause: error });
+  }
+}
+
