fe34b1355e73823d77e4e057cf79e63bbdcd9141
Daniel Medrano Huerta <daniel@quicktortoise.com>
2026-04-03 09:10:26 -0600

    Add SignalR integration for dynamic turn updates
    - Added `@microsoft/signalr` dependency.
    - Updated `TurnDisplay` to establish a SignalR connection.
    - Implemented real-time updates for turn queue and assignments.
    - Refactored `TurnAssignment` model (`station: number` to `station: string`).

--- src/Frontend/src/models/TurnAssignment.ts
+++ src/Frontend/src/models/TurnAssignment.ts
@@ -3,5 +3,5 @@ import type { AgentRole } from "./AgentRole.ts";
 export interface TurnAssignment {
   turn: string;
   type: AgentRole;
-  station: number;
+  station: string;
 }
--- src/Frontend/src/pages/TurnDisplay.tsx
+++ src/Frontend/src/pages/TurnDisplay.tsx
@@ -2,39 +2,86 @@ import { getAssignations, getTurns } from "../api/turnsApi.ts";
 import { useEffect, useState } from "react";
 import type { TurnAssignment } from "../models/TurnAssignment.ts";
 import { AgentRole } from "../models/AgentRole.ts";
+import * as signalR from "@microsoft/signalr";
 
 export default function TurnDisplay() {
-  const [currentTurn] = useState<TurnAssignment | null>(null);
+  const [currentTurn, setCurrentTurn] = useState<TurnAssignment | null>(null);
   const [ejecutivoTurns, setEjecutivoTurns] = useState<TurnAssignment[]>([]);
   const [ventanillaTurns, setVentanillaTurns] = useState<TurnAssignment[]>([]);
   const [queue, setQueue] = useState<number>(0);
+  const TURN_ASSIGNED = "turnAssigned";
+  const TURN_CREATED = "turnCreated";
+
+  async function fetchQueue() {
+    try {
+      const turns = await getTurns();
+      setQueue(turns.totalTurns);
+    } catch (error) {
+      console.error("Error fetching turns:", error);
+    }
+  }
+
+  async function fetchTurns() {
+    try {
+      const turns = await getAssignations();
+      setEjecutivoTurns(turns.find(t => t.type === AgentRole.EXECUTIVE)?.assignations ?? []);
+      setVentanillaTurns(turns.find(t => t.type === AgentRole.WINDOW)?.assignations ?? []);
+    } catch (error) {
+      console.error("Error fetching assignations:", error);
+    }
+  }
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const turns = await getTurns();
-        setQueue(turns.totalTurns);
-      } catch (error) {
-        console.error("Error fetching turns:", error);
-      }
+    console.log("useEffect");
+    const API_URL: string = import.meta.env.VITE_API_URL;
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl(`${API_URL}/ws/turns`)
+      .configureLogging(signalR.LogLevel.Information)
+      .withAutomaticReconnect()
+      .build();
 
-      try {
-        const turns = await getAssignations();
-        setEjecutivoTurns(turns.find(t => t.type === AgentRole.EXECUTIVE)?.assignations ?? []);
-        setVentanillaTurns(turns.find(t => t.type === AgentRole.WINDOW)?.assignations ?? []);
-      } catch (error) {
-        console.error("Error fetching assignations:", error);
-      }
-    }
+    connection.on(TURN_CREATED, async () => {
+      await fetchQueue();
+    });
 
-    fetchData();
+    connection.on(TURN_ASSIGNED, async (turn: string, type: string, station: string) => {
+      if (type === AgentRole.EXECUTIVE)
+        setCurrentTurn({ turn: turn, type: type, station: station });
+      else if (type === AgentRole.WINDOW)
+        setCurrentTurn({ turn: turn, type: type, station: station });
+      else {
+        setCurrentTurn(null);
+      }
+      await fetchTurns();
+    });
+
+    connection.start()
+      .then(() => {
+      })
+      .catch(err => {
+        console.error("Error during start", err);
+      });
+
+    fetchQueue().then(() => {
+    });
+    fetchTurns().then(() => {
+    });
+
+    return () => {
+      connection.off(TURN_CREATED);
+      connection.off(TURN_ASSIGNED);
+      connection.stop().catch(() => {
+      });
+    };
   }, []);
 
   return (
     <div className="container">
       <h1>Recent turns</h1>
       {currentTurn &&
-        <div className="alert alert-primary">Turn: {currentTurn.turn} at {currentTurn.type} {currentTurn.station}</div>
+        <div className="alert alert-primary text-center">
+          Turn: {currentTurn.turn} at {currentTurn.type} {currentTurn.station}
+        </div>
       }
       <div className="row">
         <div className="col">
